7f65a345baa9d203d21817d34577e8621e735884
juanfcogarciauceda <48983515+juanfcogarciauceda@users.noreply.github.com>
2019-04-30 19:59:39 +0200

    Update interfazeliminar.cpp

--- interfaz/interfazeliminar.cpp
+++ interfaz/interfazeliminar.cpp
@@ -8,7 +8,7 @@ InterfazEliminar::InterfazEliminar(QWidget *parent) :
 {
     ui->setupUi(this);
     ui->eliminado->setVisible(false);
-    //ui->noencontrado->setVisible(false);
+    ui->noencontrado->setVisible(false);
 }
 
 InterfazEliminar::~InterfazEliminar()
@@ -21,11 +21,11 @@ void InterfazEliminar::on_ok_clicked()
     QString filtro = ui->filtro->text(); string filtrox = filtro.toStdString();
     eliminar elimino=eliminar();
     int cont = elimino.eliminar_datos(filtrox);
-//    if(cont==1){
+    if(cont==1){
         ui->eliminado->setVisible(true);
-//    }else{
-//        ui->noencontrado->setVisible(true);
-//    }
+    }else{
+        ui->noencontrado->setVisible(true);
+    }
 
 }
 
