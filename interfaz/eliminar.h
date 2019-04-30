3eb2c3aa4dd8e11a33e7e109cdad258d5ca44e9e
juanfcogarciauceda <48983515+juanfcogarciauceda@users.noreply.github.com>
2019-04-30 20:01:39 +0200

    Create eliminar.h

---
+++ interfaz/eliminar.h
@@ -0,0 +1,18 @@
+#ifndef ELIMINAR_H
+#define ELIMINAR_H
+
+#include <iostream>
+#include <vector>
+#include "leer.h"
+#include "escribir.h"
+
+using namespace std;
+
+class eliminar
+{
+public:
+    eliminar();
+    int eliminar_datos(string filtro);
+};
+
+#endif // ELIMINAR_H
