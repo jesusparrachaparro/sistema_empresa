4dafd15a3995b54d91fdaaa9675d3a13b1648d5a
juanfcogarciauceda <48983515+juanfcogarciauceda@users.noreply.github.com>
2019-04-30 20:03:19 +0200

    Create escribir.h

---
+++ interfaz/escribir.h
@@ -0,0 +1,18 @@
+#ifndef ESCRIBIR_H
+#define ESCRIBIR_H
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+class escribir
+{
+public:
+    escribir();
+    int escribir_ficheros(vector <string> profesion, vector <string> nombre, vector <int> edad, vector <string> dni, vector <string> sede, vector <float> salario, vector <string> laboratorio, vector <string> zona, vector <string> universidad, vector <int> curso, vector <string> carrera, vector <int> meses);
+
+};
+
+#endif // ESCRIBIR_H
