5cd2524eb5716fac8c5874ad56201c273895736b
juanfcogarciauceda <48983515+juanfcogarciauceda@users.noreply.github.com>
2019-05-02 19:04:41 +0200

    Update interfazordenes.cpp

--- interfaz/interfazordenes.cpp
+++ interfaz/interfazordenes.cpp
@@ -1,6 +1,7 @@
 #include "interfazordenes.h"
 #include "ui_interfazordenes.h"
 
+
 InterfazOrdenes::InterfazOrdenes(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::InterfazOrdenes)
@@ -13,17 +14,22 @@ InterfazOrdenes::~InterfazOrdenes()
     delete ui;
 }
 
+
 void InterfazOrdenes::on_crear_clicked()
 {
-    QString marca = ui->marca->text();
-    QString modelo = ui->modelo->text();
-    QString memoria = ui->memoria->text();
-    QString procesador = ui->procesador->text();
-    QString potencia = ui->potencia->text();
-    QString pines = ui->pines->text();
-    QString material = ui->material->text();
-    QString largo = ui->largo->text();
-    QString ancho = ui->ancho->text();
-    QString grosor = ui->grosor->text();
-    QString operario = ui->operario->text();
+    QString marca = ui->marca->text(); string marcax = marca.toStdString();
+    QString modelo = ui->modelo->text(); string modelox = modelo.toStdString();
+    QString memoria = ui->memoria->text(); string memoriax = memoria.toStdString();
+    QString procesador = ui->procesador->text(); string procesadorx = procesador.toStdString();
+    QString potencia = ui->potencia->text(); string potenciax = potencia.toStdString();
+    QString pines = ui->pines->text(); string pinesx = pines.toStdString();
+    QString material = ui->material->text(); string materialx = material.toStdString();
+    QString largo = ui->largo->text(); string largox = largo.toStdString();
+    QString ancho = ui->ancho->text(); string anchox = ancho.toStdString();
+    QString grosor = ui->grosor->text(); string grosorx = grosor.toStdString();
+    QString operario = ui->operario->text(); string operariox = operario.toStdString();
+
+    ordenes orden = ordenes();
+    orden.nueva_orden(marcax, modelox, memoriax, procesadorx, potenciax, pinesx, materialx, largox, anchox, grosorx, operariox);
+
 }
