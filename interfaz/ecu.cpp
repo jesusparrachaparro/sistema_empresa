c4ccb4f256cec3ddf7091374f2ecccd51d2ed273
juanfcogarciauceda <48983515+juanfcogarciauceda@users.noreply.github.com>
2019-05-02 19:05:38 +0200

    Update ecu.cpp

--- interfaz/ecu.cpp
+++ interfaz/ecu.cpp
@@ -1,4 +1,5 @@
 #include "ecu.h"
+#include <QDebug>
 
 // Constructor de ECU
 Ecu::Ecu(string marca, string modelo, Pcb pcb, Carcasa carcasa){
@@ -9,8 +10,42 @@ Ecu::Ecu(string marca, string modelo, Pcb pcb, Carcasa carcasa){
 }
 
 // Métodos de ECU
-void Ecu::mostrarMateriales(){
-    cout<<"Marca: "<<marca<<"\nModelo: "<<modelo<<endl;
-    pcb.mostrarPCB();
-    carcasa.mostrarCarcasa();
+void Ecu::mostrarMateriales(string filtro){
+    fstream fichero;
+    fichero.open("C:\\Users\\USUARIO\\Desktop\\orden.txt",ios::out | ios::app);
+
+    vector<string> profesion; vector <string> nombre; vector <int> edad; vector <string> dni; vector <string> sede; vector <float> salario; vector <string> laboratorio; vector <string> zona; vector <string> universidad; vector <int> curso; vector <string> carrera; vector <int> meses;
+
+    leer leo_fichero=leer();
+    leo_fichero.leer_ficheros(&profesion, &nombre, &edad, &dni, &sede, &salario, &laboratorio, &zona, &universidad, &curso, &carrera, &meses);
+
+    int cont = 0;
+    unsigned posicion;
+    for(unsigned i=0;i<dni.size();i++){
+        if(filtro==dni[i]){
+            posicion=i;
+            cont++;
+        }
+    }
+
+    Operario o(nombre[posicion], edad[posicion], dni[posicion], salario[posicion], zona[posicion]);
+
+    if(cont==1){
+        fichero<<"Nueva orden:\t\t<<Fecha y hora: ";
+
+        auto t = time(nullptr);
+        auto tm = localtime(&t);
+        fichero << put_time(tm, "%d-%m-%Y %H-%M-%S") << endl;
+        fichero.close();
+
+        o.mostrarNombre();
+
+        fstream fichero;
+        fichero.open("C:\\Users\\USUARIO\\Desktop\\orden.txt",ios::out | ios::app);
+        fichero<<"-Datos de la ECU: \n\tMarca: "<<marca<<"\n\tModelo: "<<modelo<<endl;
+        fichero.close();
+
+        pcb.mostrarPCB();
+        carcasa.mostrarCarcasa();
+    }
 }
