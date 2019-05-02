7f7445c4005e170defe15f7dc1b1048143f43f4b
juanfcogarciauceda <48983515+juanfcogarciauceda@users.noreply.github.com>
2019-05-02 19:10:07 +0200

    Update pcb.cpp

--- interfaz/pcb.cpp
+++ interfaz/pcb.cpp
@@ -14,5 +14,9 @@ Pcb::Pcb(string memoria, string procesador, int potencia, int pines){
 
 // Métodos PCB
 void Pcb::mostrarPCB(){
-    cout<<"Memoria: "<<memoria<<"\nProcesador: "<<procesador<<"\nPotencia: "<<potencia<<"\nNúmero de pines: "<<pines<<endl;
+    fstream fichero;
+    fichero.open("C:\\Users\\USUARIO\\Desktop\\orden.txt",ios::out | ios::app);
+    fichero<<"-Datos de la PCB: \n\tMemoria: "<<memoria<<"\n\tProcesador: "<<procesador<<"\n\tPotencia: "<<potencia<<"\n\tNúmero de pines: "<<pines<<endl;
+    fichero.close();
 }
+
